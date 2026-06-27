314e332bc139654470bf9b011a7ce8b4ac3f51f4
StachowiakDawid <dawidos234@gmail.com>
2026-06-27 23:17:24 +0200

    Update pad.sh, add new firmware

--- firmware/pad.sh
+++ firmware/pad.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 dd if=$1 of=$2 bs=1 count=$(($(wc -c $1 | cut -d' ' -f2)-28))
 dd if=$1 of="$2_end" bs=1 skip=$(($(wc -c $1 | cut -d' ' -f2)-28))
-truncate -s 4194276 $2
-cat "$2_end" >> $2
+dd if=/dev/zero of=zero_file bs=1 count=$3
+truncate -s $((4194304-28-$3)) $2
+cat $2 >> zero_file
+cat "$2_end" >> zero_file
+mv zero_file $2
 rm "$2_end"
 
 
